a14f24ca19b78d9f791ded5309c0674648060a88
T0madon <joao.daciuk@gmail.com>
2025-06-13 15:45:22 -0300

    adicionando critérios no PayerRepository

--- src/main/groovy/com/mini/asaas/payer/PayerRepository.groovy
+++ src/main/groovy/com/mini/asaas/payer/PayerRepository.groovy
@@ -1,7 +1,42 @@
 package com.mini.asaas.payer
 
+import com.mini.asaas.repository.Repository
 import grails.compiler.GrailsCompileStatic
+import org.grails.datastore.mapping.query.api.BuildableCriteria
 
 @GrailsCompileStatic
-class PayerRepository  {
+class PayerRepository implements Repository<Payer, PayerRepository> {
+
+    @Override
+    void buildCriteria() {
+        addCriteria {
+            if (search.containsKey("customerId")) {
+                eq("customer.id", Long.valueOf(search.customerId.toString()))
+            }
+
+            if (search.containsKey("cpfCnpj")) {
+                eq("cpfCnpj", search.cpfCnpj)
+            }
+
+            if (search.containsKey("email")) {
+                eq("email", search.email)
+            }
+
+        }
+
+    }
+
+    @Override
+    BuildableCriteria getBuildableCriteria() {
+        return Payer.createCriteria()
+    }
+
+    @Override
+    List<String> listAllowedFilters() {
+        return [
+                "cpfCnpj",
+                "email",
+                "customerId"
+        ]
+    }
 }
