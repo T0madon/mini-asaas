c3bc04f58a66a37b81d34f77e4c55509ff0bbd70
sofiassoares <sofiasoares116@gmail.com>
2025-06-17 17:33:12 -0300

    feat: criando AuthController e o fluxo inicial da autenticação

---
+++ grails-app/controllers/com/mini/asaas/auth/AuthController.groovy
@@ -0,0 +1,26 @@
+package com.mini.asaas
+
+import grails.plugin.springsecurity.annotation.Secured
+
+@Secured(['permitAll'])
+class AuthController {
+
+    AuthService authService
+    static allowedMethods = [login: 'GET', authenticate: 'POST']
+
+    def login() {
+        render view: 'login'
+    }
+
+    def authenticate() {
+        try {
+            User user = authService.authenticate(params.email, params.password)
+            session.user = user
+            redirect controller: 'customer', action: 'show', id: user.customer.id
+        } catch (AuthException e) {
+            flash.message = e.message
+            flash.email = params.email
+            redirect action: 'login'
+        }
+    }
+}
