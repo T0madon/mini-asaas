39b0c93d0d42409c1a32f9d0e3aa9e3e8731311a
T0madon <joao.daciuk@gmail.com>
2025-06-24 14:48:04 -0300

    refact: organizando if do CpfCnpjUtils

--- src/main/groovy/com/mini/asaas/utils/CpfCnpjUtils.groovy
+++ src/main/groovy/com/mini/asaas/utils/CpfCnpjUtils.groovy
@@ -50,8 +50,12 @@ class CpfCnpjUtils {
     }
 
     public static String formatCpfCnpj(String cpfCnpj) {
-        if (isCpf(cpfCnpj)) return cpfCnpj.replaceAll(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
-        else if (isCnpj(cpfCnpj)) return cpfCnpj.replaceAll(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5')
+        if (isCpf(cpfCnpj)) {
+            return cpfCnpj.replaceAll(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+        }
+        else if (isCnpj(cpfCnpj)) {
+            return cpfCnpj.replaceAll(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5')
+        }
 
         return null
     }
