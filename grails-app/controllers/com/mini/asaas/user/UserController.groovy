824be3e42d7589570f5a69a5e7a24ec36ac63d3b
sofiassoares <sofiasoares116@gmail.com>
2025-07-04 14:28:42 -0300

    refact: melhorando o escopo no UserController

--- grails-app/controllers/com/mini/asaas/user/UserController.groovy
+++ grails-app/controllers/com/mini/asaas/user/UserController.groovy
@@ -18,21 +18,20 @@ class UserController {
     }
 
     def save() {
-        Object jsonData = request.JSON
-        Map jsonDataAsMap = jsonData as Map
-
-        CustomerSaveAdapter customerAdapter = new CustomerSaveAdapter(jsonDataAsMap)
-        UserSaveAdapter userAdapter = new UserSaveAdapter(jsonDataAsMap)
-
         try {
+            Object jsonData = request.JSON
+            Map jsonDataAsMap = jsonData as Map
+            CustomerSaveAdapter customerAdapter = new CustomerSaveAdapter(jsonDataAsMap)
+            UserSaveAdapter userAdapter = new UserSaveAdapter(jsonDataAsMap)
+
             customerService.createAccount(customerAdapter, userAdapter)
             render(status: 201, contentType: 'application/json') {
                 [status: "SUCCESS", message: "Conta criada com sucesso!"]
             }
 
-        } catch (Exception e) {
+        } catch (Exception exception) {
             render(status: 400, contentType: 'application/json') {
-                [status: "ERROR", message: "Erro no cadastro: ${e.message}"]
+                [status: "ERROR", message: "Erro no cadastro: ${exception.message}"]
             }
         }
     }
