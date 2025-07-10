575445942ed468766987ffa70b77dbc1fe943658
T0madon <joao.daciuk@gmail.com>
2025-07-10 14:27:24 -0300

    refact: alterando nomes dos métodos no EmailService

--- grails-app/services/com/mini/asaas/payment/PaymentService.groovy
+++ grails-app/services/com/mini/asaas/payment/PaymentService.groovy
@@ -31,7 +31,7 @@ class PaymentService {
 
         payment.customer = customer
         payment.save(failOnError: true)
-        emailService.emailPaymentCreated(payment)
+        emailService.notifyPaymentCreated(payment)
 
         return payment
     }
@@ -69,7 +69,7 @@ class PaymentService {
         payment.deleted = true
         payment.status = PaymentStatus.CANCELED
         payment.save(failOnError: true)
-        emailService.emailPaymentDeleted(payment)
+        emailService.notifyPaymentDeleted(payment)
     }
 
     public void restore(Long customerId, Long id) {
@@ -94,7 +94,7 @@ class PaymentService {
 
         payment.status = PaymentStatus.RECEIVED
         payment.paymentDate = new Date()
-        emailService.emailPaymentReceive(payment)
+        emailService.notifyPaymentReceive(payment)
 
         payment.save(failOnError: true)
     }
@@ -113,7 +113,7 @@ class PaymentService {
                     Payment payment = PaymentRepository.get(id)
                     payment.status = PaymentStatus.OVERDUE
                     payment.save(failOnError: true)
-                    emailService.emailPaymentOverdue(payment)
+                    emailService.notifyPaymentOverdue(payment)
                 } catch (Exception exception) {
                     log.error("Erro ao processar pagamento", exception)
                     throw new RuntimeException("Erro ao processar pagamento!" + exception)
